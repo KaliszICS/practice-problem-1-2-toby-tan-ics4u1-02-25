1a07052c6b8009b46b573efc1ca9ba715aba01b6
toby-tan-ics4u1-02-25 <toby.tan@student.tdsb.on.ca>
2025-04-03 21:15:05 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,29 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-	
+	public static boolean validIndex(int[] arr, int num){
+		try{
+			int num2 = arr[num];
+			return true;
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			return false;
+		}
+	}
+	public static int divide(int huzz, int buzz) {
+        try {
+            return huzz / buzz;
+        } catch (ArithmeticException e) {
+            return 0;
+        }
+    }
+	public static int safeConvertStringtoInt(String str){
+		try{
+			return Integer.parseInt(str);
+		}
+		catch(NumberFormatException e){
+			return 0;
+		}
+	}
 
 }
